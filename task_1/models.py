41daa52debec4eaae0f0a3cefbf872f62728123a
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-05-20 17:03:48 +0500

    add models

---
+++ task_1/models.py
@@ -0,0 +1,12 @@
+from unittest.mock import Base
+
+from sqlalchemy import Column, Integer, String, DateTime
+
+
+class QuizQuestion(Base):
+    __tablename__ = 'quiz_questions'
+
+    id = Column(Integer, primary_key=True, index=True)
+    question_text = Column(String)
+    answer_text = Column(String)
+    created_date = Column(DateTime)
