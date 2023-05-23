7d87e6d98ca4a5c2d0840fa7f681f30a34ff1ff9
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-05-23 21:46:27 +0500

    refac task_2

--- task_2/main.py
+++ task_2/main.py
@@ -1,101 +1,82 @@
+import os
 import uuid
 
-from fastapi import UploadFile, HTTPException, FastAPI
-from pydantic import BaseModel
-import ffmpeg
+from fastapi import FastAPI, HTTPException, UploadFile, Depends, File
 from fastapi.responses import FileResponse
+from pydub import AudioSegment
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from models import User, AudioRecord
-from database import SessionLocal, Base, engine
+from models import Base, User, AudioRecord
+
+DATABASE_URL = 'postgresql://postgres:postgres@db/postgres'
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-class CreateUserRequest(BaseModel):
-    name: str
+@app.post('/users')
+def create_user(user_data: dict, db=Depends(get_db)):
+    name = user_data.get('name')
+    if not name:
+        raise HTTPException(status_code=400, detail='Name is required')
 
-
-class CreateUserResponse(BaseModel):
-    id: int
-    access_token: str
-
-
-class AddAudioRequest(BaseModel):
-    user_id: int
-    access_token: str
-    audio: UploadFile
-
-
-class AddAudioResponse(BaseModel):
-    url: str
-
-
-class GetAudioResponse(BaseModel):
-    url: str
-
-
-def convert_to_mp3(input_filename, output_filename):
-    ffmpeg.input(input_filename).output(output_filename, audio_bitrate='128k').run()
-
-
-@app.post('/users', response_model=CreateUserResponse)
-def create_user(user_request: CreateUserRequest):
-    session = SessionLocal()
-    user = User(name=user_request.name, access_token=str(uuid.uuid4()))
-    session.add(user)
-    session.commit()
-    session.refresh(user)
+    access_token = str(uuid.uuid4())
+    user = User(name=name, access_token=access_token)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
     return {'id': user.id, 'access_token': user.access_token}
 
 
-@app.post('/audio', response_model=AddAudioResponse)
-def add_audio(audio_request: AddAudioRequest):
-    session = SessionLocal()
-    user = session.query(User).filter(User.id == audio_request.user_id,
-                                      User.access_token == audio_request.access_token).first()
+@app.post('/records')
+def upload_record(user_id: int, access_token: str, record: UploadFile = File(...), db=Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id, User.access_token == access_token).first()
     if not user:
-        raise HTTPException(status_code=404, detail='User not found')
+        raise HTTPException(status_code=401, detail='Invalid user credentials')
 
-    audio = audio_request.audio
-    audio_id = str(uuid.uuid4())
+    record_id = str(uuid.uuid4())
+    record_path = f'{record_id}.wav'
+    mp3_path = f'{record_id}.mp3'
 
-    # Save the WAV file
-    wav_filename = f'recordings/{audio_id}.wav'
-    with open(wav_filename, 'wb') as f:
-        f.write(audio.file.read())
+    # Save the uploaded record as a WAV file
+    with open(record_path, 'wb') as f:
+        f.write(record.file.read())
 
-    # Convert to MP3
-    mp3_filename = f'recordings/{audio_id}.mp3'
-    convert_to_mp3(wav_filename, mp3_filename)
+    # Convert the WAV file to MP3 format
+    audio = AudioSegment.from_wav(record_path)
+    audio.export(mp3_path, format='mp3')
 
-    # Save the record in the database
-    record = AudioRecord(user_id=user.id, filename=wav_filename, mp3_filename=mp3_filename)
-    session.add(record)
-    session.commit()
-    session.refresh(record)
+    # Save the record details in the database
+    download_url = f'http://localhost:8000/record?id={record_id}&user={user_id}'
+    audio_record = AudioRecord(user_id=user_id, record_id=record_id, download_url=download_url)
+    db.add(audio_record)
+    db.commit()
 
-    # Return the download URL
-    download_url = f'http://localhost:8000/audio/{record.id}'
-    return {'url': download_url}
+    return {'download_url': download_url}
 
 
-@app.get('/audio/{record_id}', response_model=GetAudioResponse)
-def get_audio(record_id: int):
-    session = SessionLocal()
-    record = session.query(AudioRecord).filter(AudioRecord.id == record_id).first()
-    if not record:
-        raise HTTPException(status_code=404, detail='Audio record not found')
+@app.get('/record')
+def download_record(id: str, user: int, db=Depends(get_db)):
+    audio_record = db.query(AudioRecord).filter(AudioRecord.record_id == id, AudioRecord.user_id == user).first()
+    if not audio_record:
+        raise HTTPException(status_code=404, detail='Record not found')
 
-    return {'url': record.mp3_filename}
+    file_path = f'{id}.mp3'
+    file_name = os.path.basename(file_path)
 
+    headers = {
+        'Content-Disposition': f'attachment; filename="{file_name}"'
+    }
 
-@app.get('/audio/download/{record_id}')
-def download_audio(record_id: int):
-    session = SessionLocal()
-    record = session.query(AudioRecord).filter(AudioRecord.id == record_id).first()
-    if not record:
-        raise HTTPException(status_code=404, detail='Audio record not found')
-
-    return FileResponse(record.mp3_filename, media_type='audio/mpeg')
+    return FileResponse(file_path, headers=headers)
--- task_2/models.py
+++ task_2/models.py
@@ -8,14 +8,14 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True)
-    access_token = Column(String, unique=True)
+    name = Column(String, unique=True, index=True)
+    access_token = Column(String, unique=True, index=True)
 
 
 class AudioRecord(Base):
     __tablename__ = 'audio_records'
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer)
-    filename = Column(String)
-    mp3_filename = Column(String)
+    user_id = Column(Integer, index=True)
+    record_id = Column(String, unique=True, index=True)
+    download_url = Column(String, unique=True, index=True)
